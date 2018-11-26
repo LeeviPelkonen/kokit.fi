b50ec530c6635797f09ad3017611e08f97328b70
LeeviPelkonen <Leevi.pelkonen@metropolia.fi>
2018-11-26 15:32:11 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-  $('#paavalikko li:first-child').click(function(evt) {
-    evt.preventDefault();
-    $('#paavalikko li:not(:first)').slideToggle(400);
-  });
-</script>
-<script>
- var vierivaMenu = function() {
-    if ($(window).scrollTop() > 400) {
-        // console.log($(window).scrollTop());
-        var mainWidth = $('main').width();
-        var mainPos = $('main').position();
-        //console.log(Number(mainPos.left)+16);
-        var leveys = Number(mainWidth)/4+8;
-        $('#paavalikko').attr('style', 'position:fixed;  top:0;  left: '+Number(mainPos.left+4)+'px; width: '+leveys+'px;');
-        //$('#palkki').attr('style', 'height: 4rem;');
-    } else {
-            $('#paavalikko').attr('style', 'position:static;');
-            //$('#palkki').attr('style', 'height: 4rem;');
-    }
- }
-  $(window).scroll(vierivaMenu);
-  $(window).resize(vierivaMenu);
-</script>
+function openSeach() {
+  var x = document.getElementById("advsearch");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
