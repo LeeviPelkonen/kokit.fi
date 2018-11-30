3bac4398f5e990316af2f2facda950d45f0904c1
lasto <lassiy@metropolia.fi>
2018-11-30 11:03:34 +0200

    Register added to login

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 document.getElementById('login').addEventListener("click", close);
 document.getElementById('logbutton').addEventListener("click", show);
+document.getElementById('cancelbtn').addEventListener("click", reg)
 
 function show() {
   document.getElementById('id01').style.display="block";
@@ -9,6 +10,11 @@ function close() {
   document.getElementById('id01').style.display="none";
 }
 
+function reg() {
+  document.getElementById('id01').style.display="none";
+  document.getElementById('id02').style.display="block";
+}
+
 // Get the modal
 const modal = document.getElementById('id01');
 
@@ -16,5 +22,15 @@ const modal = document.getElementById('id01');
 window.onclick = event => {
   if (event.target === modal) {
     modal.style.display = "none";
+  }
+};
+
+// Get the modal
+const modal2 = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = event => {
+  if (event.target === modal2) {
+    modal2.style.display = "none";
   }
 };
