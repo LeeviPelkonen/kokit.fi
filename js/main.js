29efc394ce0287fcfc2ed9b46e12367dd4e0e6cd
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2018-12-04 10:43:30 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ function openMenu(evt) {
   var x = document.querySelectorAll(".advnav");
   x.forEach((li) => {
       if (li.classList.contains('hidden')) {
-          console.log(li.firstChild.classList);
           li.classList.add('visible');
           li.classList.remove('hidden');
       } else {
@@ -42,9 +41,9 @@ function decreaseValue() {
   value--;
   document.getElementById('number').value = value;
 }
+
 document.getElementById('burger').addEventListener('click',openMenu);
 document.getElementById('openSearch').addEventListener('click',openSearch);
-
 
 const addButton = document.getElementById("increase");
 addButton.addEventListener("click", (evt) => {
