ac15350098b743b56463230273732c80e9fecbf1
lasto <lassiy@metropolia.fi>
2018-12-04 10:57:26 +0200

    getting up to date

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-function openSearch() {
-  var x = document.getElementById("advsearch");
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
+function openSearch(evt) {
+  evt.preventDefault();
+  var x = document.querySelectorAll(".advsearch");
+  x.forEach((li)=>{
+  if (li.classList.contains('hiddenFull')) {
+    li.classList.add('visibleFull');
+    li.classList.remove('hiddenFull');
+} else {
+    li.classList.add('hiddenFull');
+    li.classList.remove('visibleFull');
+    }
+  })
+}
+
+function openMenu(evt) {
+  evt.preventDefault();
+  var x = document.querySelectorAll(".advnav");
+  x.forEach((li) => {
+      if (li.classList.contains('hidden')) {
+          li.classList.add('visible');
+          li.classList.remove('hidden');
+      } else {
+          li.classList.remove('visible');
+          li.classList.add('hidden');
+      }
+  })
 }
 
 function increaseValue() {
+  console.log('working');
   var value = parseInt(document.getElementById('number').value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
@@ -21,3 +41,17 @@ function decreaseValue() {
   value--;
   document.getElementById('number').value = value;
 }
+
+document.getElementById('burger').addEventListener('click',openMenu);
+document.getElementById('openSearch').addEventListener('click',openSearch);
+
+const addButton = document.getElementById("increase");
+addButton.addEventListener("click", (evt) => {
+  console.log(document.getElementById("increase"));
+  increaseValue();
+});
+
+addButton.addEventListener("mousedown", (evt) => {
+  addButton.classList.add("animationbutton");
+  console.log(addButton);
+});
