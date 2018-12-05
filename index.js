e58a75acb4b888605f42e290056d163eb6de0d0a
visaso <visaso@metropolia.fi>
2018-12-05 11:17:55 +0200

    Refactor project files

---
+++ index.js
@@ -0,0 +1,142 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const db = require('./modules/database');
+const resize = require('./modules/resize');
+const exif = require('./modules/exif');
+
+const bodyParser = require('body-parser');
+
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads/'});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const connection = db.connect();
+
+const cb = (result, res) => {
+  console.log(result);
+  res.send(result);
+};
+
+app.use(express.static('public'));
+
+// respond to post and save file
+app.post('/upload', upload.single('mediafile'), (req, res, next) => {
+  next();
+});
+
+// create thumbnail
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 300,
+      './public/thumbnails/' + req.file.filename + '_thumb').then(() => {
+    next();
+  });
+});
+
+// create medium image
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 640,
+      './public/medium/' + req.file.filename + '_medium').then(() => {
+    next();
+  });
+});
+
+// get coordinates
+app.use('/upload', (req, res, next) => {
+  exif.getCoordinates(req.file.path).then(coords => {
+    req.coordinates = coords;
+    next();
+  }).catch(() => {
+    console.log('No coordinates');
+    req.coordinates = {};
+    next();
+  });
+});
+
+// insert to database
+app.use('/upload', (req, res, next) => {
+  const data = [
+    req.body.category,
+    req.body.title,
+    req.body.details,
+    req.coordinates,
+    req.file.filename + '_thumb',
+    req.file.filename + '_medium',
+    req.file.filename
+  ];
+  db.insert(data, connection, next);
+});
+
+// get updated data form database and send to client
+app.use('/upload', (req, res) => {
+  db.select(connection, cb, res);
+});
+
+app.get('/images', (req, res) => {
+  db.select(connection, cb, res);
+});
+/*
+app.patch('/images', (req, res) => {
+  const data = [
+    req.body.category,
+    req.body.title,
+    req.body.details,
+    req.body.mID,
+  ];
+  db.update(data, connection).then((result) => {
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+*/
+app.delete('/images', (req, res) => {
+  const item = [req.body.mID];
+  db.deleteImage(item, connection);
+});
+
+app.get('/search', (req, res) => {
+  db.searchTitle([req.query.title], connection).then((result) => {
+    res.send(result);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+app.listen(3000);
+
+// *********************
+// esimerkkejä:
+/*
+app.get('/', (req, res) => {
+  console.log(req.ip);
+  console.log(req.query.myParam);
+  res.send('ok 1');
+});
+
+app.get('/path1/:param1', (req, res) => {
+  console.log(req.params.param1);
+  res.send('ok 2');
+});
+
+app.get(['/path2', '/path3', '/path4'], (req, res) => {
+  console.log(req);
+  res.send('ok 3');
+});
+
+app.use('/json', (req, res, next) => {
+  console.log('Middleware tässä');
+  next();
+});
+
+app.get('/json', (req, res) => {
+  const objekti = {
+    id: 1,
+    name: 'My response',
+  };
+  res.send(objekti);
+});
+*/
---
+++ modules/database.js
@@ -0,0 +1,80 @@
+'use strict';
+const mysql = require('mysql2');
+
+const connect = () => {
+  // create the connection to database
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+  });
+  return connection;
+};
+
+const select = (connection, callback, res) => {
+  // simple query
+  connection.query(
+      'SELECT * FROM kokit',
+      (err, results, fields) => {
+        // console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+        console.log(err);
+        callback(results, res);
+      },
+  );
+};
+
+const insert = (data, connection, callback) => {
+  // simple query
+  connection.execute(
+      'INSERT INTO kokit (category, title, details, coordinates, thumbnail, image, original) VALUES (?, ?, ?, ?, ?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        // console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+        console.log(err);
+        callback();
+      },
+  );
+};
+
+const update = (data, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'UPDATE kokit SET category = ?, title = ?, details = ? WHERE mID = ?;', data, (err, results, fields) => {
+          if (err) reject(err);
+          if (results) resolve(results);
+        }
+    )
+  });
+};
+
+const deleteImage = (item, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'DELETE FROM kokit WHERE mID = ?;' , item, (err, results, fields) => {
+          resolve(results);
+        }
+    )
+  })
+};
+
+const searchTitle = (title, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'SELECT * FROM kokit WHERE title = ?;', title, (err, results, fields) => {
+          resolve(results);
+        },
+    )
+  })
+};
+
+module.exports = {
+  connect: connect,
+  select: select,
+  insert: insert,
+  update: update,
+  deleteImage: deleteImage,
+  searchTitle: searchTitle,
+};
