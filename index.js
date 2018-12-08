b8e4328f592448543777797382fee5857c372bde
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2018-12-08 16:21:47 +0200

    Trying to get passporet work here

---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const db = require('./modules/database');
+const resize = require('./modules/resize');
+//const exif = require('./modules/exif');
+
+const bodyParser = require('body-parser');
+
+const multer = require('multer');
+const upload = multer({dest: './public/uploads/'});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({encoded: true}));
+app.use(bodyParser.json());
+
+const connection = db.connect();
+
+const cb = (result, res) => {
+  console.log(result);
+  res.send(result);
+};
+
+app.use(express.static('public'));
+
+app.post('/upload', upload.single('mediafile'), (req, res, next) => {
+  console.log('/upload happens now');
+  console.log('req.query', req.query);
+  next();
+});
+
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 300,
+      './public/thumbnails/' + req.file.filename + '_thumb').then(() => {
+    next();
+  });
+});
+
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 640,
+      './public/medium/' + req.file.filename + '_medium').then(() => {
+    next();
+  });
+});
+
+app.use('/upload', (req, res, next) => {
+  console.log(req.body);
+  const data = [
+    //req.body.rID,
+    req.body.UserID,
+    req.body.Name,
+    req.body.Ingredients,
+    req.file.filename,
+    req.file.filename + '_thumb',
+    req.file.filename + '_medium',
+    req.body.PreparationTime,
+
+  ];
+  db.insert(data, connection, next);
+});
+
+app.use('/upload', (req, res) => {
+  db.select(connection, cb, res);
+});
+
+app.get('/images', (req, res) => {
+  db.selectRecipe(connection, cb, res);
+});
+
+app.get('/user/:id', function(req, res) {
+  console.log('user ' + req.params.id);
+  db.selectUser(connection, [req.params.id], (response) => {
+    res.send(response);
+  });
+});
+
+/*
+app.patch('/images', (req, res) => {
+	const data = [
+		req.body.category,
+		req.body.title,
+		req.body.details,
+		req.body.mID,
+		];
+	db.update(data,connection).then((result) => {
+	}).catch((err) => {
+		console.log(err);
+	});
+});
+
+app.delete('/images', (req, res) => {
+	const item = [req.body.mID];
+	db.deleteImage(item, connection);
+});
+*/
+app.get('/search', (req, res) => {
+  db.searchTitle([req.query.title], connection).then((result) => {
+    res.send(result);
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
+const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+
+const options = {
+  key: sslkey,
+  cert: sslcert,
+};
+
+//app.listen(8000);
+http.createServer((req, res) => {
+  res.writeHead(301,
+      {'Location': 'https://' + req.headers.host + '/nodekek' + req.url});
+  res.end();
+}).listen(8000);
+console.log('listening port: 3000');
+https.createServer(options, app).listen(3000);
---
+++ modules/database.js
@@ -0,0 +1,107 @@
+'use strict';
+const mysql = require('mysql2');
+
+const connect = () => {
+  // create the connection to database
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+  });
+  return connection;
+};
+
+const select = (connection, callback, res) => {
+  // simple query
+  connection.query(
+      'SELECT * FROM users;',
+      (err, results, fields) => {
+        // console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+        console.log(err);
+        console.log(results);
+        callback(results, res);
+      },
+  );
+};
+
+const selectRecipe = (connection, callback, res) => {
+  connection.query(
+      'SELECT * FROM recipe WHERE rID = 3;',
+      (err, results, fields) => {
+        console.log(err);
+        console.log(results);
+        callback(results, res);
+      },
+  );
+};
+
+const selectUser = (connection, data, callback) => {
+  connection.execute(
+      'SELECT * FROM users WHERE uID = ?;',
+      data,
+      (err, results, fields) => {
+        console.log(err);
+        console.log(results);
+        callback(results);
+      },
+  );
+};
+
+const insert = (data, connection, callback) => {
+  // simple query
+  connection.execute(
+      'INSERT INTO recipe (rUSERID, rNAME, rINGREDIENTS, rPICTURE, rTHUMBNAIL, rMEDIUM , rPREPTIME) VALUES (?, ?, ?, ?, ?, ?, ?);',
+      data,
+      (err, results, fields) => {
+        // console.log(results); // results contains rows returned by server
+        // console.log(fields); // fields contains extra meta data about results, if available
+        console.log(err);
+        callback();
+      },
+  );
+};
+/*
+const update = (data, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'UPDATE kokit SET category = ?, title = ?, details = ? WHERE mID = ?;', data, (err, results, fields) => {
+          if (err) reject(err);
+          if (results) resolve(results);
+        }
+    )
+  });
+};
+
+const deleteImage = (item, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'DELETE FROM kokit WHERE mID = ?;' , item, (err, results, fields) => {
+          resolve(results);
+        }
+    )
+  })
+};
+
+const searchTitle = (title, connection) => {
+  return new Promise((resolve, reject) => {
+    connection.execute(
+        'SELECT * FROM kokit WHERE title = ?;', title, (err, results, fields) => {
+          resolve(results);
+        },
+    )
+  })
+};
+*/
+
+module.exports = {
+  connect: connect,
+  select: select,
+  selectRecipe: selectRecipe,
+  selectUser: selectUser,
+  insert: insert,
+  //update: update,
+  //deleteImage: deleteImage,
+  //searchTitle: searchTitle,
+};
