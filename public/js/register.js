246f0ac9c9cf0a9d2748205e4e6171f901a924df
lasto <lassiy@metropolia.fi>
2018-12-10 14:30:45 +0200

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const form = document.getElementById('register');
+const shii = document.getElementById('register');
 const password = document.getElementById("password")
   , confirm_password = document.getElementById("confirm_password");
 
 const registerForm = (evt) => {
     evt.preventDefault();
-    ff = new FormData(form);
+    ff = new FormData(shii);
     const settings = {
         method: 'post',
         body: ff,
