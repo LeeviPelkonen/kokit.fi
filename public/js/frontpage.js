10bd7ce7a110803042ea0f97e2aa3e84cc95171a
LeeviPelkonen <Leevi.pelkonen@metropolia.fi>
2018-12-10 14:35:02 +0200

    search working perhaps

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -50,7 +50,7 @@ const getRecipes = (x) => {
                 console.log('searching recipe by name');
                 console.log((json[x].rNAME));
                 console.log((title));
-                if ((json[x].rNAME).includes(title)) {
+                if ((json[x].rNAME.toLowerCase).includes(title.toLowerCase)) {
                     console.log('sfound one');
                     const li = document.createElement('li');
                     const title = document.createElement('h3');
