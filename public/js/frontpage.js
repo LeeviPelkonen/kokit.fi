f98cec8f5537f66667c9caee1ba02de2f76775a8
LeeviPelkonen <Leevi.pelkonen@metropolia.fi>
2018-12-10 13:50:54 +0200

    test

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const list = document.querySelector('#recipelist');
+const searchButton = document.querySelector('#searchButton');
+const title;
 
-const getRecipes = () => {
+const getRecipes = (x) => {
     const settings = {
         method: 'get',
       };
@@ -13,25 +15,27 @@ const getRecipes = () => {
     list.innerHTML = '';
     let x = 0;
     json.forEach((rPICTURE) => {
-      const li = document.createElement('li');
-      const title = document.createElement('h3');
-      const time = document.createElement('h3');
-      title.innerHTML = json[x].rNAME;
-      time.innerHTML = prepTime(json[x].rPREPTIME);
-      li.appendChild(title);
-      li.appendChild(time);
-      const img = document.createElement('img');
-      img.src = './thumbnails/' + json[x].rTHUMBNAIL;
-      const id = json[x].rID;
-      img.addEventListener('click', () => { 
-        openRecipe(id);
-      });
-      title.addEventListener('click', () => { 
-        openRecipe(id);
-      });
-      console.log(json[x].rTHUMBNAIL);
-      li.appendChild(img);
-      list.appendChild(li);
+        if (json[x].rNAME.includes(x)) {
+            const li = document.createElement('li');
+            const title = document.createElement('h3');
+            const time = document.createElement('h3');
+            title.innerHTML = json[x].rNAME;
+            time.innerHTML = prepTime(json[x].rPREPTIME);
+            li.appendChild(title);
+            li.appendChild(time);
+            const img = document.createElement('img');
+            img.src = './thumbnails/' + json[x].rTHUMBNAIL;
+            const id = json[x].rID;
+            img.addEventListener('click', () => {
+                openRecipe(id);
+            });
+            title.addEventListener('click', () => {
+                openRecipe(id);
+            });
+            console.log(json[x].rTHUMBNAIL);
+            li.appendChild(img);
+            list.appendChild(li);
+        };
       x++;
     });
   });
@@ -51,3 +55,11 @@ const openRecipe= (x) => {
   sessionStorage.setItem('recipe', x);
   window.location.pathname = '/nodekek/recipedetails.html';
 }
+
+const searchRecipes = () => {
+    title = document.querySelector('#searchKeyword').value;
+    getRecipes(title);
+};
+
+
+searchButton.addEventListener('click', searchRecipes);
