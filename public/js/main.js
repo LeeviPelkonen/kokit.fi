27ef165590c8ef9ef53a57641fe401e6acb10630
lasto <lassiy@metropolia.fi>
2018-12-10 10:41:53 +0200

    Added password check

--- public/js/main.js
+++ public/js/main.js
@@ -89,7 +89,19 @@ function exit() {
   document.getElementById('id02').style.display="none";
 }
 
+const password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
 
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
 
 login.addEventListener("click", close);
 logbutton.addEventListener("click", show);
